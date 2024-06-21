e88398c04ff6a86da3f967d00ba36fda78eeb62a
oryango <76882639+oryango@users.noreply.github.com>
2024-06-22 06:40:12 +0800

    Adds Card Replacement assistant
    Signed-off-by: oryango <76882639+oryango@users.noreply.github.com>

---
+++ imports/api/classes/server/vapiTools/CardReplacement.js
@@ -0,0 +1,88 @@
+import { FuncTemplate } from "./template";
+import { Consumer } from "../../../DB";
+
+class CardReplacement extends FuncTemplate {
+    constructor(async, server, messages, func, meta) {
+        super(async, server, messages, func, meta);
+    }
+
+    verifyRequest() {
+        const data = this.Data;
+
+        if (data) {
+            const consumer = new Consumer(data);
+            return consumer.processCardReplacement();
+        }
+        return Promise.resolve({ verified: false });
+    }
+
+    parseRequest(request) {
+        return this.verifyRequest().then(({ verified, result }) => {
+            if(verified) {
+                this.setResponse(200, {
+                    results: [
+                        {
+                            toolCallId: request.message.toolCalls[0].id,
+                            result: `Process complete. The date ranges for delivery is ${JSON.stringify(result)}`,
+                        },
+                    ],
+                }, true);
+            } else {
+                this.setResponse(200, {
+                    results: [
+                        {
+                            toolCallId: request.message.toolCalls[0].id,
+                            result: "Process failed. Please try again later",
+                        },
+                    ],
+                });
+            }
+        }).catch((e) => {
+            console.log(e);
+            this.setResponse(200, {
+                results: [
+                    {
+                        toolCallId: request.message.toolCalls[0].id,
+                        result: "Process failed. Please try again later",
+                    },
+                ],
+            });
+        }).finally(() => this.checkResponse());
+    }
+}
+const card = {
+    type: "function",
+    messages: [
+        {
+            type: "request-start",
+            content: "Processing request...",
+        },
+        {
+            type: "request-response-delayed",
+            content: "Just a second...",
+            timingMilliseconds: 2000,
+        },
+    ],
+    function: {
+        name: "process_card_replacement",
+        parameters: {
+            type: "object",
+            properties: {
+                otp: {
+                    type: "string",
+                },
+            },
+        },
+        description:
+            "Process card replacement and returns possible delivery dates.",
+    },
+    async: false,
+    server: {
+        url: "https://kind-intensely-herring.ngrok-free.app/card_replacement",
+    },
+};
+const meta = {
+    title: "Process card replacement",
+};
+
+export default new CardReplacement(card.async, card.server, card.messages, card.function, meta);
--- imports/api/classes/server/vapiTools/assistants/CardReplacement.js
+++ imports/api/classes/server/vapiTools/assistants/CardReplacement.js
@@ -1,5 +1,5 @@
 export default {
-    "name": "Task Router",
+    "name": "Card Replacement",
     "firstMessageMode": "assistant-speaks-first",
     "firstMessage": "Hello, How can I help you today?",
     "recordingEnabled": true,
@@ -20,14 +20,14 @@ export default {
     },
     "model": {
         "provider": "openai",
-        "model": "gpt-3.5-turbo",
+        "model": "gpt-4o",
         "temperature": 0.7,
         "maxTokens": 525,
         "emotionRecognitionEnabled": false,
         "messages": [
             {
                 "role": "system",
-                "content": "[Identity]\nYou are an assistant that will handle calls from customer. You will be responsible for transferring calls either in Full Verification 5 Assistant, Card Replacement OR in Partial Verification Assistant.\n[Rules]\n- do not answer or ask questions that is not past of your scope and identity\n- you need to follow the steps in correct order\n- make your response short and concise\n[Task]\n1. Ask them if they want to transfer the call from to full verification assistant, Card Replacement or partial verification assistant.\nConditions:\n - before transferring the call to assistant, provide the destination to server \n - if they choose full verification, transfer it to Full Verification 5 Assistant\n - if they choose partial verification, transfer the call to Partial Verification Assistant\n - if they choose partial verification, transfer the call to Partial Verification Assistant\n - if they do not want to transfer the call, end the call in formal way."
+                "content": "You are a voice assistant. That will process a customer's card replacement. You need to perform a series of steps in order and only move to the next step if the current step is verified.undefined1. Send a one-time password. use the function send_otp. <wait for user response>undefined2. Verify the one time password and make sure to only allow 3 retries. use the function verify_otp.undefined3. If successful, process the card replacement. use the function verify_otp.undefined4. Inform user of possible delivery times from result."
             }
         ],
         "tools": []
